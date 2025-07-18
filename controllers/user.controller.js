cbfa776b777031ba5f6014a3060ec8ca76bcbf22
LaxmiKoushik N <itskoushik007@gmail.com>
2025-07-18 22:40:49 +0530

    Changes password api added

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -134,6 +134,45 @@ exports.loginUser = async (req, res) => {
   }
 };
 
+exports.changePassword = async (req, res) => {
+  const userId = req.user.id;
+  const { currentPassword, newPassword } = req.body;
+
+  if (!currentPassword || !newPassword) {
+    return res.status(400).json({ message: "Both fields are required." });
+  }
+
+  try {
+    const [rows] = await pool.query(
+      "SELECT password_hash FROM users WHERE id = ?",
+      [userId]
+    );
+
+    if (rows.length === 0)
+      return res.status(404).json({ message: "User not found." });
+
+    const isMatch = await bcrypt.compare(
+      currentPassword,
+      rows[0].password_hash
+    );
+    if (!isMatch)
+      return res
+        .status(401)
+        .json({ message: "Current password is incorrect." });
+
+    const hashed = await bcrypt.hash(newPassword, 10);
+    await pool.query("UPDATE users SET password_hash = ? WHERE id = ?", [
+      hashed,
+      userId,
+    ]);
+
+    res.status(200).json({ message: "Password changed successfully." });
+  } catch (err) {
+    console.error("Change password error:", err);
+    res.status(500).json({ message: "Server error." });
+  }
+};
+
 exports.getUserProfile = async (req, res) => {
   const userId = req.user.id; // decoded from JWT token
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -5,13 +5,14 @@ const {
   loginUser,
   getUserProfile,
   verifyOtp,
+  changePassword,
 } = require("../controllers/user.controller");
 
 const { verifyToken } = require("../middleware/authMiddleware");
 router.post("/auth/verify", verifyOtp);
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-
+router.post("/change-password", verifyToken, changePassword);
 router.get("/me", verifyToken, (req, res) => {
   res.json({ message: "Protected route accessed", user: req.user });
 });
